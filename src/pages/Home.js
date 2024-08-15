095a245119163f0d498bfff6625a66ce735ac03f
minsu kim <kimminsu31415@gmail.com>
2024-08-15 23:33:18 +0900

    feat: 테스트 데이터 연결

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,37 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
-function Home() {
-  return <div>여기는 홈 페이지입니다.</div>;
-}
+const Home = () => {
+  const navigate = useNavigate();
+
+  const handleSelectCharacterGender = (gender) => {
+    //gender에 따라 데이터를 pageA로 보냄과 동시에 pageA로 이동
+    const characters =
+      gender === "female" ? ["여자 캐릭터 데이터"] : ["남자 캐릭터 데이터"];
+
+    navigate("/PageA", { state: { characters } });
+  };
+
+  return (
+    <div>
+      <div>여기는 main 페이지</div>
+
+      <button
+        className="bg-blue-300"
+        onClick={() => handleSelectCharacterGender("female")}
+      >
+        여자 캐릭터 선택
+      </button>
+
+      <button
+        className="bg-blue-100"
+        onClick={() => handleSelectCharacterGender("male")}
+      >
+        남자 캐릭터 선택
+      </button>
+    </div>
+  );
+};
 
 export default Home;
